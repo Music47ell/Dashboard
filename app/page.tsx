e2ca923d9ea59dcee57cb3bfac7ca07ed657e484
Ahmet ALMAZ <Music47ell@gmail.com>
2024-01-02 22:16:07 +0300

    chore: remove stats and restore components

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import { SectionContainer } from '@/components/UI'
 import { PageTitle } from '@/components/UI'
 import siteMetadata from '@/data/siteMetadata'
 
-import OverviewItem from './components/OverviewItem'
+// import OverviewItem from "./components/OverviewItem";
 import SectionHeading from './components/SectionHeading'
 import SectionSubHeading from './components/SectionSubHeading'
 import Codes from './components/codes'
-// import Music from './components/music'
-// import Shows from "./components/shows";
+import Music from './components/music'
+import Shows from './components/shows'
 
 export const metadata: Metadata = {
 	title: `Dashboard - ${siteMetadata.title}`,
@@ -18,25 +18,25 @@ export const metadata: Metadata = {
 }
 
 export default async function Homepage() {
-	const [views, stats, subscribers] = await Promise.all([
-		fetch(`${siteMetadata.siteUrl}/api/views`, {
-			next: {
-				revalidate: 3600
-			}
-		}).then((res) => res.json()),
+	// const [views, stats, subscribers] = await Promise.all([
+	// 	fetch(`${siteMetadata.siteUrl}/api/views`, {
+	// 		next: {
+	// 			revalidate: 3600
+	// 		}
+	// 	}).then((res) => res.json()),
 
-		fetch(`${siteMetadata.siteUrl}/api/stats`, {
-			next: {
-				revalidate: 86400
-			}
-		}).then((res) => res.json()),
+	// 	fetch(`${siteMetadata.siteUrl}/api/stats`, {
+	// 		next: {
+	// 			revalidate: 86400
+	// 		}
+	// 	}).then((res) => res.json()),
 
-		fetch(`${siteMetadata.siteUrl}/api/stats/newsletter`, {
-			next: {
-				revalidate: 604800
-			}
-		}).then((res) => res.json())
-	])
+	// 	fetch(`${siteMetadata.siteUrl}/api/stats/newsletter`, {
+	// 		next: {
+	// 			revalidate: 604800
+	// 		}
+	// 	}).then((res) => res.json())
+	// ])
 
 	return (
 		<SectionContainer>
@@ -59,17 +59,17 @@ export default async function Homepage() {
 				</SectionSubHeading>
 			</section>
 			<div className="grid grid-cols-1 gap-3 lg:grid-cols-2 xl:grid-cols-2">
-				<OverviewItem label="All Posts Views" value={views.toLocaleString()} />
+				{/* <OverviewItem label="All Posts Views" value={views.toLocaleString()} />
 				<OverviewItem label="Number of Posts" value={stats.numberOfPosts} />
 				<OverviewItem label="Number of Words" value={stats.numberOfWords.toLocaleString()} />
-				<OverviewItem label="Newsletter Subscribers Count" value={subscribers.toLocaleString()} />
+				<OverviewItem label="Newsletter Subscribers Count" value={subscribers.toLocaleString()} /> */}
 			</div>
 			<hr className="my-6 border-gray-700" />
 			<Codes />
 			<hr className="my-6 border-gray-700" />
-			{/* <Music /> */}
+			<Music />
 			<hr className="my-6 border-gray-700" />
-			{/* <Shows /> */}
+			<Shows />
 		</SectionContainer>
 	)
 }
